32b22234fbae6426781152daf8c411a07d09bd10
YutaNamba <ynanbo423@gmail.com>
2026-05-30 21:39:20 +0900

    feat:

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "manakana-shogi-terraform-state-apne1"
+    bucket       = "manakana-shogi-terraform-state-apne"
     key          = "matching-server-shogi/sandbox/terraform.tfstate"
     region       = "ap-northeast-1"
     encrypt      = true
